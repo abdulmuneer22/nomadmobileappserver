d8c161e6b2a0d2432d0d5a73b390ba662bf5a5d7
Muneer PP <abdulmuneer22@gmail.com>
2017-09-26 21:16:50 +0530

    graphql -- init

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+// import bodyParser from 'body-parser';
+import schema from './schema';
+
+const graphqlHTTP = require('express-graphql');
+
 const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res) => {
-  res.send('Hello');
-});
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
 app.listen(4000);
